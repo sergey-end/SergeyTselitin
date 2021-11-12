df18524f52988b6833620793d5cfa41e4494c278
sergey-end <13optimist@mail.ru>
2021-11-12 21:34:45 +0300

    pro

---
+++ home/Graduation project/js/script.js
@@ -0,0 +1,41 @@
+function changeColor() {
+    var EL = document.getElementsByClassName('slider-img_left');
+    EL[0].style.animation = 'swing 2.5s';
+    EL[0].style.animationFillMode = 'forwards'
+    EL[1].style.animation = 'swing 2.5s';
+    EL[1].style.animationFillMode = 'forwards'
+    EL[2].style.animation = 'swing 2.5s';
+    EL[2].style.animationFillMode = 'forwards'
+    EL[3].style.animation = 'swing 2.5s';
+    EL[3].style.animationFillMode = 'forwards' 
+}
+
+function colorChange() {
+    var SR = document.getElementsByClassName('slider-img_left');
+    SR[0].style.animation = 'right 2.5s';
+    SR[0].style.animationFillMode = 'forwards'
+    SR[1].style.animation = 'right 2.5s';
+    SR[1].style.animationFillMode = 'forwards'
+    SR[2].style.animation = 'right 2.5s';
+    SR[2].style.animationFillMode = 'forwards'
+    SR[3].style.animation = 'right 2.5s';
+    SR[3].style.animationFillMode = 'forwards'
+}
+
+function elementOffset() {
+    var AA = document.getElementsByClassName('about-us-img');
+    AA[0].style.animation = 'banOne 2.5s';
+    AA[0].style.animationFillMode = 'forwards'
+    AA[1].style.animation = 'banOne 2.5s';
+    AA[1].style.animationFillMode = 'forwards'
+    console.log('about-us-img')
+}
+
+function elementSet() {
+    var AA = document.getElementsByClassName('about-us-img');
+    AA[0].style.animation = 'banTwo 2.5s';
+    AA[0].style.animationFillMode = 'forwards'
+    AA[1].style.animation = 'banOTwo 2.5s';
+    AA[1].style.animationFillMode = 'forwards'
+    console.log('about-us-img')
+}
